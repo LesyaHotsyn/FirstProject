8c1142a307cd67cea8824a3e131a93e058307184
LesyaHotsyn <85835454+LesyaHotsyn@users.noreply.github.com>
2022-08-29 17:56:00 +0300

    Add files via upload

--- js/bxSlider.js
+++ js/bxSlider.js
@@ -1,7 +1,7 @@
 $('.').bxSlider({
-    // mode: 'fade',
-    // controls: false,
-    // pager: true,
-    // randomStart: true
+    mode: 'fade',
+    controls: false,
+    pager: true,
+    randomStart: true
 });
 
