a880de38c9467745a6066d7b63fc29740e5130e0
LesyaHotsyn <85835454+LesyaHotsyn@users.noreply.github.com>
2022-08-29 21:54:47 +0300

    Add files via upload

--- js/upButton.js
+++ js/upButton.js
@@ -8,5 +8,14 @@ $(window).on('scroll', function(){
 
 $('#up').on('click', function(){
     $('html,body').animate({scrollTop: 0},500)
+});
+
+$('#about_link').on('click',function(e){
+    e.preventDefault();
+    $('html,body').animate({scrollTop: $('#about').offset().top}, 1000)
 })
 
+$('#inbox_link').on('click',function(e){
+    e.preventDefault();
+    $('html,body').animate({scrollTop: $('#inbox').offset().top}, 1000)
+})
